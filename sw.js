32554279391da5b83f0cb35a989a5d8bee480940
khantmeet8-dotcom <khantmeet8@gmail.com>
2026-09-22 00:50:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,15 @@
-const CACHE = "scoop-manager-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('scoop-store').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json'
+    ]))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match("./SCOOP_MANAGER_PWA.html"))
-    )
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
